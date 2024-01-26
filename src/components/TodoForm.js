3b82577877c48966ff7f5edcbb0893a077ea55fd
Joeroberts <josekama777@gmail.com>
2024-01-26 19:11:57 +0300

    clear after submission in the input field

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import React, {useState} from 'react'
 
 export const TodoForm = ({addTodo}) => {
-  const [input, setInput] = useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addTodo(input);
-  }
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();
+        if (value) {
+          // add todo
+          addTodo(value);
+          // clear form after submission
+          setValue('');
+        }
+      };
   return (
-    <form className ="TodoForm" onSubmit={handleSubmit}>
-        <input type="text" className='todo-input' placeholder='tasks' onChange={(e) => setInput(e.target.value)}/>
-        <button type="submit" className='todo-btn'>Add Task</button>
-    </form>
-    
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Enter tasks here' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
   )
 }
